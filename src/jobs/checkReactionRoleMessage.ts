35e6487eaed6804e555874e6c3fd8eac19a96459
MikanNigata <yukichiba2101@gmail.com>
2026-04-23 15:45:57 +0900

    chore: drop unrelated reaction-role changes from vc fix PR

--- src/jobs/checkReactionRoleMessage.ts
+++ src/jobs/checkReactionRoleMessage.ts
@@ -3,12 +3,6 @@ import { Client } from "discord.js";
 const reactionRoleChannelID = process.env["REACTIONROLE_CHANNEL_ID"]!;
 const botID = process.env["BOT_ID"]!;
 
-const REACTION_ROLE_MESSAGE = [
-  "リアクションしてロールを付与しよう！",
-  ":bell: : 通知勢 : たくさん通知が届くよ！",
-  ":sound: : VC募集 : VCに参加したい人向け！",
-].join("\n");
-
 export default async function checkReactionRoleMessage(
   client: Client,
 ): Promise<string | null> {
@@ -25,19 +19,22 @@ export default async function checkReactionRoleMessage(
 
     const messages = await channel.messages.fetch({ limit: 10 });
 
-    const targetMessage = messages.find(
-      (m) =>
-        m.author.id === botID &&
-        m.content.includes("リアクションしてロールを付与しよう！"),
-    );
+    const targetMessage = messages.find((m) => {
+      m.author.id === botID && m.content.includes("ロールを付与");
+    });
 
     if (targetMessage) {
       return targetMessage.id;
+    } else {
+      const sentMessage = await channel.send(`
+                                             リアクションしてロールを付与しよう！
+                                             :bell: : 通知勢 : たくさん通知が届くよ！
+                                             :sound: : VC募集 : VCに参加したい人向け！
+                                             `);
+
+      return sentMessage.id;
     }
-
-    const sentMessage = await channel.send(REACTION_ROLE_MESSAGE);
-    return sentMessage.id;
+  } else {
+    throw new Error("This channel can't send msg");
   }
-
-  throw new Error("This channel can't send msg");
 }
